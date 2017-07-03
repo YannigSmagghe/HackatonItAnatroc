71c1068f8fc5511aef3bb6ae83634928e7ff3ea7
Alexis Bussières <bussieres.alexis.dev@gmail.com>
2017-07-03 23:37:26 +0200

    --wip-- [skip ci]

--- app/api.anatroc/app/AppKernel.php
+++ app/api.anatroc/app/AppKernel.php
@@ -13,8 +13,10 @@ class AppKernel extends Kernel
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+//            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
             new AppBundle\AppBundle(),
         ];
 
