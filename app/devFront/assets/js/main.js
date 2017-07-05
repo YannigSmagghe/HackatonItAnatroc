a473eb5d5b306e3a7fafbe2af76787b74ca47695
ayolia18 <ayolia18@hotmail.fr>
2017-07-05 16:51:15 +0200

    recup info fichier JSON

--- app/devFront/assets/js/main.js
+++ app/devFront/assets/js/main.js
@@ -1,4 +1,17 @@
 $( document ).ready(function() {
+
+    $.getJSON( "test.json", function( data ) {
+        console.log(data.data[0].type);
+        console.log(data.data[0].data.temperature);
+        console.log(data.data[0].data.ville);
+
+        $('.result_type').text(data.data[0].type);
+        $('.result_temperature').text(data.data[0].data.temperature+'°');
+        $('.result_ville').text(data.data[0].data.ville);
+    });
+
+
+
     $("#menu_connexion").click(function() {
         $(".input-container").hide();
         $(".connexion-container").show();
