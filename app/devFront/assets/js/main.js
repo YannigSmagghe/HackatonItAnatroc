b7b8d123a24979d20a0e1f5cea57821cc5c88c50
apprenant <smagghe.yannig@gmail.com>
2017-07-05 14:18:54 +0200

    voice activation

--- app/devFront/assets/js/main.js
+++ app/devFront/assets/js/main.js
@@ -1,1 +1,14 @@
-console.log('test');
+//send input
+function getInputTimer() {
+    // $("#search-input").hide();
+    //
+}
+var timer = null;
+$("#search-input").on("keyup", function() {
+    console.log(timer);
+    if (timer) {
+        clearTimeout(timer); //cancel the previous timer.
+    }
+    timer = setTimeout(getInputTimer, 3000);
+    return timer;
+});
