6c5dd8126e26a0fb0a6f78fbebcd0c2f15851c79
Alexis Bussières <bussieres.alexis.dev@gmail.com>
2017-07-06 12:25:22 +0200

    Remove api SubwayTCL for GoogleDirection

--- app/api.anatroc/src/AppBundle/Controller/DefaultController.php
+++ app/api.anatroc/src/AppBundle/Controller/DefaultController.php
@@ -2,7 +2,7 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Api\Subway\SubwayTCL;
+use AppBundle\Api\Transport\GoogleDirection;
 use AppBundle\Model\ApiData;
 use AppBundle\Resolver\ApiServiceResolver;
 use JMS\Serializer\SerializerBuilder;
@@ -21,13 +21,13 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // Simulation of user input to retrieve related services from his keywords
-        $services = $this->get(ApiServiceResolver::class)->resolveByApiKeyWords(['metro', 'meteo', 'slip']);
+        $services = $this->get(ApiServiceResolver::class)->resolveByApiKeyWords(['voiture', 'meteo', 'slip']);
 
         $apiData = new ApiData();
         $apiData->setType(self::API_DATA_TYPE);
         foreach ($services as $service) {
-            if ($service instanceof SubwayTCL) {
-                $data = $this->get(SubwayTCL::class)->getStations();
+            if ($service instanceof GoogleDirection) {
+                $data = $this->get(GoogleDirection::class)->getDirection();
                 $apiData->addData($data);
             }
         }
