d936c25969ae5050e42dc682cb55b6c410a7ff29
timmyfirst <timmyc71@gmail.com>
2017-07-07 15:36:15 +0200

    Commentaires ajoutés au code, sur WeatherInfoClimat

--- app/api.anatroc/src/AppBundle/Api/Weather/WeatherInfoClimat.php
+++ app/api.anatroc/src/AppBundle/Api/Weather/WeatherInfoClimat.php
@@ -12,6 +12,10 @@ use AppBundle\Model\Weather\WeatherData;
 
 class WeatherInfoClimat extends AbstractWeather
 {
+    /**
+     * @return WeatherData
+     * Requête et retourne les infos météo
+     */
     public function getWeather()
     {
         $response = $this->getGuzzle()->get('http://www.infoclimat.fr/public-api/gfs/json?_ll=45.77987,4.88471&_auth=UUsCFQ5wV3VVeFdgBnBWf1U9ADULfQIlBnoFZgtuAH1UP1AxUjIHYVM9WyYDLAUzBShXNAA7CTkFbgV9Xy1UNVE7Am4OZVcwVTpXMgYpVn1VewBhCysCJQZmBWULeABiVDFQPVIvB2RTOVs%2BAy0FMwU0VzIAIAkuBWcFZl87VD5RMgJuDmpXPVU5VzEGKVZ9VWMAaQsxAmsGYQVhCzIAMlQ%2FUGJSMgc0U21bMAMtBTMFMlczADsJNwVjBWJfO1QoUS0CHw4eVyhVeld3BmNWJFV7ADULagJu&_c=027a5f6d83c9c484d1f0ba18810b275f');
@@ -21,6 +25,12 @@ class WeatherInfoClimat extends AbstractWeather
 
         return $this->transformToWeatherData($weather);
     }
+
+    /**
+     * @param $json
+     * @return WeatherData
+     * Récupère les données du json pour les mettre dans un objet qui sera ajouté au json global retourné
+     */
 
     private function transformToWeatherData($json)
     {
@@ -41,6 +51,7 @@ class WeatherInfoClimat extends AbstractWeather
 
     /**
      * @return array
+     * Obtient la bonne date à requêter
      */
     public function getDate($result)
     {
@@ -65,6 +76,7 @@ class WeatherInfoClimat extends AbstractWeather
 
     /**
      * @return array
+     * Obtient la météo actuelle suivant la date actuelle
      */
     public function getWeatherByDate($data)
     {
@@ -75,6 +87,7 @@ class WeatherInfoClimat extends AbstractWeather
 
     /**
      * @return \DateTime|null
+     * Retourne la date la plus proche par rapport à la date actuelle
      */
     public function getBetterDate($dateavant, $dateapres, $date)
     {
@@ -92,6 +105,12 @@ class WeatherInfoClimat extends AbstractWeather
 
     /**
      * @return integer
+     * Détermine la météo suivant les paramètres récupérés dans le json
+     * neige = boolean indiquant le risque de neige
+     * pluie = la pluie prévue en millimètres
+     * pluie_convective = la pluie convective en millimètres
+     * nebulosite = pourcentage de nuage recouvrant le ciel
+     * vent_moyen = vitesse en km/h du vent
      */
     public function getWeatherByParams($neige, $pluie, $pluie_convective, $nebulosite, $vent_moyen)
     {
