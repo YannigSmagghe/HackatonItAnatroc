98a11460e6efeddc24ab2e0299cfaeed9e7e5647
Alexis Bussières <bussieres.alexis.dev@gmail.com>
2017-07-12 21:52:47 +0200

    Modification ClientRequest allow to pass string to methods setFrom and SetTo Exclude Client folder from autowiring

--- app/api.anatroc/src/AppBundle/Client/ClientRequest.php
+++ app/api.anatroc/src/AppBundle/Client/ClientRequest.php
@@ -17,7 +17,22 @@ class ClientRequest
     /**
      * @var null|Localisation
      */
-    private $localisation = null;
+    private $currentLocalisation = null;
+
+    /**
+     * @var string
+     */
+    private $text = '';
+
+    /**
+     * @var null|Localisation|string
+     */
+    private $from = null;
+
+    /**
+     * @var null|Localisation|string
+     */
+    private $to = null;
 
     /**
      * @var array
@@ -25,23 +40,98 @@ class ClientRequest
     private $parameters = [];
 
     /**
+     * ClientRequest constructor.
+     * @param string $text
+     */
+    public function __construct(string $text)
+    {
+        $this->text = $text;
+    }
+
+    /**
+     * @return string
+     */
+    public function getText(): string
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param string $text
+     * @return ClientRequest
+     */
+    public function setText(string $text): ClientRequest
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
      * Provide a Localisation object to obtain the current latitude and longitude client
      *
      * @return Localisation|null
      */
-    public function getLocalisation(): ?Localisation
+    public function getCurrentLocalisation(): ?Localisation
     {
-        return $this->localisation;
+        return $this->currentLocalisation;
     }
 
     /**
-     * @param Localisation $localisation
-     * @return $this
+     * @param Localisation|null $currentLocalisation
+     * @return ClientRequest
      */
-    public function setLocalisation(Localisation $localisation)
+    public function setCurrentLocalisation(Localisation $currentLocalisation = null): ClientRequest
     {
-        $this->localisation = $localisation;
+        $this->currentLocalisation = $currentLocalisation;
+
         return $this;
+    }
+
+    /**
+     * @return Localisation|null|string
+     */
+    public function getFrom()
+    {
+        return $this->from;
+    }
+
+    /**
+     * @param Localisation $from
+     * @return ClientRequest
+     */
+    public function setFrom($from): ClientRequest
+    {
+        if ($from === null || is_string($from) || $from instanceof Localisation) {
+            $this->from = $from;
+
+            return $this;
+        }
+
+        throw new \UnexpectedValueException('$from must be a type of "null", "string", AppBundle\Model\Localisation');
+    }
+
+    /**
+     * @return Localisation|null|string
+     */
+    public function getTo()
+    {
+        return $this->to;
+    }
+
+    /**
+     * @param Localisation $to
+     * @return ClientRequest
+     */
+    public function setTo($to): ClientRequest
+    {
+        if ($to === null || is_string($to) || $to instanceof Localisation) {
+            $this->to = $to;
+
+            return $this;
+        }
+
+        throw new \UnexpectedValueException('$to must be a type of "null", "string", AppBundle\Model\Localisation');
     }
 
     /**
